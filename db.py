df630ff394c45d768391e86418631b4bf54fb6d2
solowiejmaciej <55091850+solowiejmaciej@users.noreply.github.com>
2022-07-18 17:52:54 +0200

    Update db.py

--- db.py
+++ db.py
@@ -2,8 +2,8 @@ from pymongo import MongoClient
 import certifi
 
 myclient = MongoClient(
-    "mongodb+srv://maciej_solowiej:Ie0VXdGa9vld28hO@raspberrypi.rs4kf.mongodb.net/?retryWrites=true&w=majority",tlsCAFile=certifi.where()
-    #"mongodb+srv://jkusiak:jkusiak@cdv.y71kg.mongodb.net/?retryWrites=true&w=majority",tlsCAFile=certifi.where()
+    "mongodb+srv:/username:password@raspberrypi.rs4kf.mongodb.net/?retryWrites=true&w=majority",tlsCAFile=certifi.where()
+
 )
 mydb=myclient['LOL']
 mycol=mydb['Champs']
